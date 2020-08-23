4f1cfc2a640641b3eb892d761309b4daecd63338
SujayGouda <70086481+SujayGouda@users.noreply.github.com>
2020-08-23 14:18:27 +0530

    Update finding-lanes.py

--- finding-lanes.py
+++ finding-lanes.py
@@ -52,7 +52,7 @@ def region_of_interest(image):
 	return masked_image
 
 
-cap = cv2.VideoCapture("test_video_or_image")
+cap = cv2.VideoCapture("test_video.mp4")
 
 while(cap.isOpened()):
 	_, frame = cap.read()
